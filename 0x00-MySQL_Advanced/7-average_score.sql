3b46b9031eebcbe502a3e6647c3c85e57c403a84
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-07-11 06:09:27 -0700

    sql storage

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -3,7 +3,7 @@
 
 DELIMITER $$
 
-CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
     DECLARE total_score INT DEFAULT 0;
 
