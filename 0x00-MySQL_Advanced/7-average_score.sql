3653bb003239650dc833c4544b0d2c8542c17283
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-07-11 06:14:41 -0700

    sql storage

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -3,15 +3,15 @@
 
 DELIMITER $$
 
-CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE total_score INT DEFAULT 0;
-
-    SELECT AVG(score) INTO total_score
-    FROM corrections WHERE corrections.user_id = user_id;
-
-    UPDATE users SET average_score = total_score
+    DECLARE avg_score INT DEFAULT 0;
+    SELECT AVG(score) INTO avg_score
+    FROM corrections
+    WHERE user_id = user_id;
+    UPDATE users
+    SET average_score = avg_score
     WHERE id = user_id;
-END $$
+END$$
 
 DELIMITER ;
