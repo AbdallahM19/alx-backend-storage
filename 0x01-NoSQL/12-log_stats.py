bbe4cbdfa5120427c14e8630b3e601ea0c7aa7af
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-07-16 10:54:47 -0700

    Log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,1 +1,28 @@
 #!/usr/bin/env python3
+"""task 12 pymongo"""
+
+from pymongo import MongoClient
+
+
+def log_stats(collection):
+    """
+    a Python script that provides
+    some stats about Nginx
+    logs stored in MongoDB.
+    """
+    total_logs = collection.count_documents({})
+    print("{} logs".format(total_logs))
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, count))
+
+    status_check = collection.count_documents({"method": "GET", "path": "/status"})
+    print("{} status check".format(status_check))
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+    log_stats(collection)
