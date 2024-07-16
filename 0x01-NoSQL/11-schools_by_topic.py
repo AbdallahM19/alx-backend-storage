56076b3715c59ad40a7db457d792a904824f6bbc
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-07-16 10:31:35 -0700

    Where can I learn Python

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+"""task 11 pymongo"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    a Python function that returns
+    the list of school having
+    a specific topic.
+    """
+    list_doc = []
+    for i in mongo_collection.find({"topics": topic}):
+        list_doc.append(i)    
+    return list_doc
