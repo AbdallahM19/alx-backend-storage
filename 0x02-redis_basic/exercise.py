6dc4b4fcd50d75652bade076649905524b919fde
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-07-17 16:00:24 -0700

    redis basic

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,8 +6,18 @@ Cache class
 
 import redis
 from uuid import uuid4
-from typing import Union, Callable
+from functools import wraps
+from typing import Union, Callable, Any
 
+
+def count_calls(method: Callable) -> Callable:
+    """Decorator to count calls to a function"""
+    @wraps(method)
+    def wrap_incr(self, *args, **kwargs) -> Any:
+        """Wrap the method to count calls"""
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return wrap_incr
 
 class Cache:
     """class cache"""
@@ -16,12 +26,13 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """store data in cache"""
         key = str(uuid4())
         self._redis.set(key, data)
         return key
-
+    
     def get(self, key: str, fn: Callable = None) -> Union[
         str, bytes, int, float]:
         """get data from cache"""
