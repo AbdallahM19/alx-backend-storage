a1885c902f0cc0d204d5aa506b7af6d4a8f76f33
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-07-17 19:26:51 -0700

    web cache 0

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,25 +6,24 @@ Web caching with Redis
 
 import redis
 import requests
-from typing import Callable
 from functools import wraps
 
 
 redis_client = redis.Redis()
 
 
-def cache_data(method: Callable) -> Callable:
+def cache_data(method):
     """Cache data in Redis"""
     @wraps(method)
-    def wrap_url(url) -> str:
+    def wrap_url(url):
         """Wrap the url to cache it in Redis"""
-        redis_client.incr('count:{}'.format(url))
-        res = redis_client.get('result:{}'.format(url))
+        res = redis_client.get('cached:{}'.format(url))
         if res:
             return res.decode('utf-8')
         res = method(url)
-        redis_client.set('count:{}'.format(url), 0)
-        redis_client.setex('result:{}'.format(url), 10, res)
+        redis_client.incr('count:{}'.format(url))
+        redis_client.set('count:{}'.format(url), res, ex=10)
+        redis_client.expire('result:{}'.format(url), 10)
         return res
     return wrap_url
 
@@ -34,3 +33,7 @@ def get_page(url: str) -> str:
     """Get page from url"""
     response = requests.get(url)
     return response.text
+
+
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
