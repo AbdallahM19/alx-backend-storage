5dd50d6385c2556c08df31982002162be0f73ce9
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-07-19 16:58:40 -0700

    expiring web cache

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -7,18 +7,20 @@ from typing import Callable
 
 
 redis_client = redis.Redis()
+"""The module-level Redis instance."""
 
 
 def data_cacher(method: Callable) -> Callable:
     """Cache data in Redis"""
     @wraps(method)
-    def wrap_url(url: str) -> str:
+    def wrap_url(url) -> str:
         """Wrap the url to cache it in Redis"""
         redis_client.incr('count:{}'.format(url))
         result = redis_client.get('result:{}'.format(url))
         if result:
             return result.decode('utf-8')
         result = method(url)
+        redis_client.set('count:{}'.format(url), 0)
         redis_client.setex('result:{}'.format(url), 10, result)
         return result
     return wrap_url
